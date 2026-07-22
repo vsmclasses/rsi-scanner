ad0a6211f4d759296957dfe27946aa301938837f
vsmclasses <vikasmasti@gmail.com>
2026-07-22 13:52:31 +0530

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,1 +1,15 @@
+import pandas as pd
+import yfinance as yf
 
+from indicators import *
+from conditions import *
+from sheets import *
+
+# ---------------------------------------
+# Load Stock List
+# ---------------------------------------
+
+with open("symbols.txt", "r") as f:
+    SYMBOLS = [x.strip() for x in f.readlines() if x.strip()]
+
+print(f"Total Stocks : {len(SYMBOLS)}")
