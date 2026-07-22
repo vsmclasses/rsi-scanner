1186b70241d4b55623457cb3655327a8c562fca0
vsmclasses <vikasmasti@gmail.com>
2026-07-22 14:57:15 +0530

    Update indicators.py

--- indicators.py
+++ indicators.py
@@ -1,34 +1,77 @@
 import pandas as pd
-import numpy as np
 
 
-# ==============================
-# RSI (Wilder RSI)
-# ==============================
+# ==========================================================
+# RSI (Wilder)
+# ==========================================================
 
 def calculate_rsi(close, period=14):
 
     delta = close.diff()
 
-    gain = delta.where(delta > 0, 0)
+    gain = delta.clip(lower=0)
 
-    loss = -delta.where(delta < 0, 0)
+    loss = -delta.clip(upper=0)
 
-    avg_gain = gain.ewm(alpha=1/period, min_periods=period, adjust=False).mean()
+    avg_gain = gain.ewm(alpha=1/period,
+                        adjust=False,
+                        min_periods=period).mean()
 
-    avg_loss = loss.ewm(alpha=1/period, min_periods=period, adjust=False).mean()
+    avg_loss = loss.ewm(alpha=1/period,
+                        adjust=False,
+                        min_periods=period).mean()
 
     rs = avg_gain / avg_loss
 
-    rsi = 100 - (100 / (1 + rs))
-
-    return rsi
+    return 100 - (100 / (1 + rs))
 
 
-# ==============================
+# ==========================================================
 # SMA
-# ==============================
+# ==========================================================
 
 def calculate_sma(close, period=7):
 
     return close.rolling(period).mean()
+
+
+# ==========================================================
+# Weekly Candle
+# ==========================================================
+
+def create_weekly(df):
+
+    weekly = pd.DataFrame()
+
+    weekly["Open"] = df["Open"].resample("W-FRI").first()
+
+    weekly["High"] = df["High"].resample("W-FRI").max()
+
+    weekly["Low"] = df["Low"].resample("W-FRI").min()
+
+    weekly["Close"] = df["Close"].resample("W-FRI").last()
+
+    weekly["Volume"] = df["Volume"].resample("W-FRI").sum()
+
+    return weekly.dropna()
+
+
+# ==========================================================
+# Monthly Candle
+# ==========================================================
+
+def create_monthly(df):
+
+    monthly = pd.DataFrame()
+
+    monthly["Open"] = df["Open"].resample("ME").first()
+
+    monthly["High"] = df["High"].resample("ME").max()
+
+    monthly["Low"] = df["Low"].resample("ME").min()
+
+    monthly["Close"] = df["Close"].resample("ME").last()
+
+    monthly["Volume"] = df["Volume"].resample("ME").sum()
+
+    return monthly.dropna()
