6f3df0398a97141f9a587fd6d2dd5a3d0117cb99
vsmclasses <vikasmasti@gmail.com>
2026-07-22 15:03:28 +0530

    Update conditions.py

--- conditions.py
+++ conditions.py
@@ -1,3 +1,5 @@
+from conditions import *
+
 def condition_1(current_rsi):
 
     return current_rsi > 50
@@ -16,3 +18,14 @@ def condition_3(weekly_rsi):
 def condition_4(monthly_rsi):
 
     return monthly_rsi > 60
+
+
+print()
+
+print("Condition 1 :", condition_1(last["RSI"]))
+
+print("Condition 2 :", condition_2(prev["RSI"]))
+
+print("Condition 3 :", condition_3(weekly_last["RSI"]))
+
+print("Condition 4 :", condition_4(monthly_last["RSI"]))
