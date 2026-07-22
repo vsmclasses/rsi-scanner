9d9d3ba535f9ebbd16cb338bb2820652d95e1e20
vsmclasses <vikasmasti@gmail.com>
2026-07-22 15:02:27 +0530

    Update conditions.py

--- conditions.py
+++ conditions.py
@@ -1,1 +1,18 @@
+def condition_1(current_rsi):
 
+    return current_rsi > 50
+
+
+def condition_2(previous_rsi):
+
+    return previous_rsi < 50
+
+
+def condition_3(weekly_rsi):
+
+    return weekly_rsi > 60
+
+
+def condition_4(monthly_rsi):
+
+    return monthly_rsi > 60
